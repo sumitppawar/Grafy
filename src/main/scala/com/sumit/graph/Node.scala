a9fa5760f3d43f717962c9b4bc2f68114ac08490
Sumit Pawar <sumitppawar@gmail.com>
2016-07-16 10:49:48 +0530

    provided defination for create Node

--- src/main/scala/com/sumit/graph/Node.scala
+++ src/main/scala/com/sumit/graph/Node.scala
@@ -1,6 +1,15 @@
 package com.sumit.graph
 
 import scala.concurrent.Future
+import com.sumit.util.GrafyConstant
+import play.api.libs.ws.WSClient
+import scala.concurrent.ExecutionContext
+import com.sumit.connection.Connection
+import com.sumit.connection.Cypher
+import com.sumit.connection.Statement
+import play.api.libs.json.JsObject
+import play.api.libs.json.Json
+import play.api.libs.json.JsArray
 
 /**
  * This class indicate Node
@@ -26,8 +35,40 @@ case class Node(id: String) {
 
 object Node {
   
-  def create(label: String, properties: Map[String, String]): Future[Option[String]] = {
-    null
+  /**
+   * Create node with give properties 
+   * @param label (Node label)
+   * @param properties (Property of node)
+   * @return It returns Node id
+ 	 */
+	def create(label: String, properties: Map[String, String])(implicit connection:Connection, executionContext: ExecutionContext,wsClient: WSClient): Future[String] = {
+	  var parameter = GrafyConstant.EMPTY_STRING
+	 
+	  //Please find createNodePost.json and createNodeReponse.json for more Info
+	  properties.keySet.foreach { key =>
+	    parameter += s"$key:{$key},";
+	  }
+	  parameter = parameter.dropRight(1)
+	  
+    val query =  s"CREATE (node:$label {$parameter}) RETURN ID(node)" 
+    val cypherObje = Cypher(Seq(Statement(query,properties)))
+    val resultFuture = connection.runCypherQuery(cypherObje)
+    
+    resultFuture.flatMap { strJson =>
+      val jsValu = Json.parse(strJson)
+      val results = (jsValu \ "results").as[JsArray]
+      
+      if((jsValu \ "errors").as[JsArray].value.size > 0) {
+       val message = (((jsValu \ "errors")(0).get) \ "message").as[String]
+       Future.failed(throw new Exception(message))
+      }else {
+        val result = results(0).get
+        val data = (result \ "data")(0).get
+        val id = (data \ "row")(0).get
+        Future.successful(id.toString())
+      }
+      
+	  }
   }
   
   def delete(id: String): Future[Boolean] = {
