2a44f5ec5196db37fdf66bd0aad1759514479813
Sumit Pawar <sumitppawar@gmail.com>
2016-07-16 10:49:08 +0530

    added more example

--- src/main/scala/com/sumit/connection/Application.scala
+++ src/main/scala/com/sumit/connection/Application.scala
@@ -3,6 +3,7 @@ package com.sumit.connection
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import play.api.libs.ws.ahc.AhcWSClient
+import com.sumit.graph.Node
 
 /**
  * @author sumit
@@ -19,32 +20,27 @@ object Application extends App {
     implicit val materializer = ActorMaterializer()
     implicit val ws = AhcWSClient()
 
-    //Build Connection 
-    val connection = Connection("neo4j", "farmer")
-
-    //Sample get Query
-    val cypher = """{
-			 "statements":[
-                			 {
-                  			 "statement":"MATCH (you:Person)  where you.authCode={authCode} RETURN you",
-                  			 "parameters":{"authCode":"jgfiuegkrp3fiugtgwfj"}
-                			 }
-			             ]
-			       }"""
-
-    //for (t <- connection.runCypherQuery(cypher)) yield println(t)
+    //Build Connection (default host is localhost, port is 7474, protocol is http
+    implicit val connection = Connection("neo4j", "farmer")
     
+    //Build connection with details (protocol: String,serverName: String,port:String,userName: String,password: String)
+    implicit val connectionWithDetails = Connection("http","localhost","7474","neo4j","farmer")
     
-    //Using Json writes for above Post Json
-    val query = "MATCH (you:Person)  where you.authCode={authCode} RETURN you"
+    //Execute CQL
+    val cqlQuery = "MATCH (n:Person) Return ID(n)"
+    for (t <- connection.runCypherQuery(cqlQuery)) yield println(t)
     
-    //Provide parameter in map
+    //Pass parameter in map  CQL (preferred)
+    val queryParametrised = "MATCH (you:Person)  where you.authCode={authCode} RETURN you"
+    //Provide parameter in map ({authcode})
     val parameters = Map("authCode"->"jgfiuegkrp3fiugtgwfj")
-    
     //build statement
-    val statement = Statement(query,parameters)
-    
+    val statement = Statement(queryParametrised,parameters)
     val cypherObje = Cypher(Seq(statement))
     
-    for (t <- connection.runCypherQuery(cypherObje)) yield println(t)
+    //for (t <- connection.runCypherQuery(cypherObje)) yield println(t)
+    
+/*    val label = "Person"
+    val prop = Map("user_name" -> "ss3333333", "authcode" -> "ss", "email" -> "33ss33333@33u55u.com", "dob"->"03/07/1991")
+    Node.create(label, prop).onComplete { x => println(x.get) }*/
 }
