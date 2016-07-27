f9ecf19a8261846c91f29cdb1834d6396a76053b
Sumit Pawar <sumitppawar@gmail.com>
2016-07-27 10:07:38 +0530

    added JsValue to Scala value mapper

--- src/main/scala/com/sumit/util/GrafyUtils.scala
+++ src/main/scala/com/sumit/util/GrafyUtils.scala
@@ -1,5 +1,8 @@
 package com.sumit.util
 import java.util.Base64
+import play.api.libs.json.JsValue
+import play.api.libs.json.JsNumber
+import play.api.libs.json.JsBoolean
 
 /**
   * Created by sumit on 5/7/16.
@@ -13,5 +16,11 @@ object GrafyUtils {
 
   def decocdeBase64(str: String = ""): String ={
     Base64.getDecoder.decode(str.getBytes()).toString
+  }
+  
+  def jsValueToScalaValue(jsValue: JsValue) = {
+    if(jsValue.isInstanceOf[JsNumber]) jsValue.asOpt[Int]
+    else if(jsValue.isInstanceOf[JsBoolean]) jsValue.asOpt[Boolean] 
+    else  jsValue.asOpt[String] 
   }
 }
